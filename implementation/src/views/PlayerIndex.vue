d77677694e7f6f27da7ea72524a1a31635760a20
ksendzo <ksenijabulatovi@gmail.com>
2021-04-05 00:52:58 +0200

    dodati pojedinacni Nav/ovi, fali dobra estetika

--- implementation/src/views/PlayerIndex.vue
+++ implementation/src/views/PlayerIndex.vue
@@ -1,4 +1,8 @@
 <template>
+<div>
+    <NavPlayer/>
+    
+
     <div class = "row"> 
         <div class = "col-8">
             <MainPlayerContent/>
@@ -7,18 +11,21 @@
             <SecondaryPlayerContent/>
         </div>
     </div>
+</div>
 </template>
 
 
 <script>
 import MainPlayerContent from '../components/player/MainPlayerContent'
 import SecondaryPlayerContent from '../components/player/SecondaryPlayerContent'
+import NavPlayer from '@/components/player/NavPlayer'
 
 export default {
     name:'PlayerIndex',
     components:{
         MainPlayerContent, 
-        SecondaryPlayerContent
+        SecondaryPlayerContent, 
+        NavPlayer
     }
 }
 </script>
