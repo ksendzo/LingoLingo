f8976a85b5ad1fb4a7782015df92ddca48cd0e08
ksendzo <ksenijabulatovi@gmail.com>
2021-05-30 10:31:47 +0200

    questions php

--- implementation/src/components/professor/NewQuestion.vue
+++ implementation/src/components/professor/NewQuestion.vue
@@ -2,21 +2,20 @@
     <div class="row">
         <div class=" col-md-12">
             <div class="card-login">
-                <form onsubmit="event.preventDefault()" class="box newQuestion">
+                <form onsubmit="event.preventDefault()" @submit.prevent='newQuestion()' class="box newQuestion">
                     <div class="row">
                         <div class="col-8 m-10">
                             <h1>New question</h1>
                             <p class="text-muted"> Please enter your new question and answer here:</p>
                         </div>
                         <div class=" col-3">
-                        <select style="width:100%;">
-                            <option>German</option>
-                            <option>Spanish</option>
+                        <select style="width:100%;" v-model="selectedLanguage">
+                                 <option v-for="language in languages" :value="language" :key="language.LanguageName">{{language.LanguageName}}</option>
                         </select>
                         </div>
                     </div>
-                    <input type="text" name="" placeholder="Question"> 
-                    <input type="text" name="" placeholder="Answer"> 
+                    <input type="text" name="" placeholder="Question" v-model="question"> 
+                    <input type="text" name="" placeholder="Answer" v-model="answer"> 
                     <input type="submit" name="" value="Submit" href="#">                    
                 </form>
             </div>
@@ -55,7 +54,38 @@
 
 <script>
 export default {
-    name:
-    "NewQuestion"
+    name: "NewQuestion",
+    data() {
+        return {
+            languages: [],
+            selectedLanguage: '',
+            question: '',
+            answer: '',
+            professor: ''
+        };
+    },
+    beforeMount(){
+        this.professor = localStorage.getItem('Username');
+        this.$guest.post('/languages')
+        .then(res => {
+            this.languages = res.data;
+        });
+    },
+    methods: {
+        newQuestion() {
+            let form = {
+                'language': this.selectedLanguage.LanguageName,
+                'question': this.question,
+                'answer': this.answer,
+                'professor': this.professor
+            }
+
+            this.$guest.put('/newQuestion', JSON.stringify(form));
+
+            this.question = '';
+            this.answer = '';
+            this.$root.$on()
+        }
+    }
 }
 </script>
